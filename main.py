5e4bbaa96287cf3203181bfb9f09ad4a350f318e
suprahit <gabriel.nogueira21@outlook.com>
2023-12-03 16:16:14 -0300

    API trocada de freecurrency para frankfurter

--- main.py
+++ main.py
@@ -1,55 +1,11 @@
 from sys import exit
+from list import currency_list
 
+url = "https://api.frankfurter.app"
+
+print("")
 print("Welcome to the currency converter")
 print("Note: Values may change overtime")
-
-
-def currency_list():
-    print("EUR - EURO")
-    print("USD - US Dollar")
-    print("JPY - Japanese Yen")
-    print("BGN - Bulgarian Lev")
-    print("CZK - Czech Republic Koruna")
-    print("DKK - Danish Krone")
-    print("GBP - British Pound Sterling")
-    print("HUF - Hungarian Forint")
-    print("PLN - Polish Zloty")
-    print("RON - Romanian Leu")
-    print("SEK - Swedish Krona")
-    print("CHF - Swiss Franc")
-    print("ISK - Icelandic Króna")
-    print("NOK - Norwegian Krone")
-    print("HRK - Croatian Kuna")
-    print("RUB - Russian Ruble")
-    print("TRY - Turkish Lira")
-    print("AUD - Australian Dollar")
-    print("BRL - Brazilian Real")
-    print("CAD - Canadian Dollar")
-    print("CNY - Chinese Yuan")
-    print("HKD - Hong Kong Dollar")
-    print("IDR - Indonesian Rupiah")
-    print("ILS - Israeli New Sheqel")
-    print("INR - Indian Rupee")
-    print("KRW - South Korean Won")
-    print("MXN - Mexican Peso")
-    print("MYR - Malaysian Riggit")
-    print("NZD - New Zealand Dollar")
-    print("PHP - Philippine Peso")
-    print("SGD - Singapore Dollar")
-    print("THB - Thai Baht")
-    print("ZAR - South African Rand")
-    print("")
-    while True:
-        print("1 - Back")
-        try:
-            escolha = int(input(">>> "))
-            if escolha == 1:
-                return
-            else:
-                raise ValueError
-        except ValueError:
-            print("Choose a valid option!")
-            print("")
 
 
 def end_app():
@@ -64,12 +20,12 @@ def menu():
         print("2 - Currency Converter")
         print("3 - End the app")
         try:
-            escolha = int(input(">>> "))
-            if escolha == 1:
-                currency_list()
-            elif escolha == 2:
+            option = int(input(">>> "))
+            if option == 1:
+                currency_list(url)
+            elif option == 2:
                 pass
-            elif escolha == 3:
+            elif option == 3:
                 end_app()
             else:
                 raise ValueError
